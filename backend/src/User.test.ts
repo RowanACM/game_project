ec864408d34267186fc7b81c20e2eb1624f5c5ea
Jason <me@jasonharrison.us>
2019-11-23 16:34:39 -0500

    Increase test coverage in User.ts

--- backend/src/User.test.ts
+++ backend/src/User.test.ts
@@ -1,8 +1,8 @@
 import { UserDBEntity } from './entities/UserDBEntity';
+import * as bcrypt from 'bcrypt';
 import User from './User';
 
 jest.mock('./entities/UserDBEntity');
-
 jest.mock('bcrypt');
 
 const mockedUserDBEntity = UserDBEntity as jest.MockedClass<typeof UserDBEntity>;
@@ -20,13 +20,23 @@ describe('Authentication', () => {
     });
   });
 
-  describe('Adding a user', () => {
+  describe('Adding and authenticating', () => {
     it('adds a user', async () => {
       const user = new User('foo');
       await user.addUser('secret');
       expect(mockedUserDBEntity).toHaveBeenCalledTimes(1);
+
       const mockedUser: any = mockedUserDBEntity.mock.instances[0];
       expect(mockedUser.save).toHaveBeenCalled();
+    });
+
+    it("checks a user's password", async () => {
+      mockedUserDBEntity.findOne = jest.fn().mockResolvedValue({ password: 'secret' });
+      Object.defineProperty(bcrypt, 'compareSync', { value: jest.fn().mockReturnValue(true) });
+
+      const user = new User('foo');
+      const isCorrectPassword = await user.checkPassword('secret');
+      expect(isCorrectPassword).toBeTruthy();
     });
   });
 });
