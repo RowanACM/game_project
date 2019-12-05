877957cef650764c35648633df0aa4673ae7738a
Jason <me@jasonharrison.us>
2019-12-05 01:35:35 -0500

    add prettier config, fix eslint

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,24 @@
 import App from 'next/app';
-import "../components/app.css"
+import '../components/app.css';
 
 class defaultApp extends App {
   render() {
     if (process.browser && process.env.NODE_ENV !== 'development') {
-      console.log('%c%s', 'color: red; background: yellow; font-size: 24px;', 'WARNING!');
-      console.log(
-        '%c%s',
-        'color: black; font-size: 18px;',
-        'Using this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.\nDo not enter or paste code that you do not understand.',
+      setTimeout(
+        console.log.bind(
+          console,
+          '%c%s',
+          'font-size:50px; font-weight:bold; color:red; -webkit-text-stroke:1px black;',
+          'Stop!',
+        ),
+      );
+      setTimeout(
+        console.log.bind(
+          console,
+          '%cUsing this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.\n%cDo not enter or paste code that you do not understand.',
+          'font-family:helvetica; font-size:20px;',
+          'font-family:helvetica; font-size:20px; font-weight:bold',
+        ),
       );
     }
     const { Component, pageProps } = this.props;
