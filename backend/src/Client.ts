db22e1d5ad4ba2dda0c454be2fb6d56d7268e856
Ryan <rywards@ymail.com>
2020-02-14 16:05:08 -0500

    added fields to client

--- backend/src/Client.ts
+++ backend/src/Client.ts
@@ -1,8 +1,68 @@
 export default class Client {
-  
+
+  uuid: string;
+  position: [number, number];
+  name: string;
+  lastAction: number;
+  room: any;
+  blockedUsers: [];
+  friends: [];
+
   constructor() {
-      
+
   }
 
-}
+  move(x: number, y: number) {
 
+  }
+
+  chat(msg: string) {
+
+  }
+
+  setRoom(room) {
+
+  }
+
+  leave() {
+
+  }
+
+  join() {
+
+  }
+
+  whisper(msg: string) {
+
+  }
+
+  isAFK() {
+
+  }
+
+  isFriend(user: any) {
+
+  }
+
+  isBlocked(user: any) {
+
+  }
+
+  block(user: any) {
+
+  }
+
+  unBlock(user: any) {
+
+  }
+
+  addFriend(user: any) {
+
+  }
+
+  unFriend(user: any) {
+
+  }
+
+
+}
