41bb6523816753f5660dcb47b7a347572c57a029
aaronspex <57270861+aaronspex@users.noreply.github.com>
2020-02-21 14:17:03 -0500

    updated form

--- frontend/components/Overlay.tsx
+++ frontend/components/Overlay.tsx
@@ -6,8 +6,24 @@ export default class Overlay extends React.Component {
 
         return (
             <div id={"overlay"}>
-                <h1 style={{color: "white", position: "relative"}}>Overlay</h1>
+             
+               
+                <h1 style={{color: "white", position: "relative"}}>Sign In:</h1>
+                <form>
+                    Username:
+                    <input type = "text" name = "username"></input>
+                    <br></br>
+
+                    Password:   
+                    <input type = "password" name = "password"></input>
+                    <br></br>
+                    <input type = "submit" name = "submit"></input>
+                </form>
+
+                
+
             </div>
+
         );
 
     }
