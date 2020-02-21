a78437b12a53a14b4291d2d2d7e9445330b9f129
aaronspex <57270861+aaronspex@users.noreply.github.com>
2020-02-21 14:55:16 -0500

    bootstrap form

--- frontend/components/Overlay.tsx
+++ frontend/components/Overlay.tsx
@@ -6,22 +6,32 @@ export default class Overlay extends React.Component {
 
         return (
             <div id={"overlay"}>
-             
-               
-                <h1 style={{color: "white", position: "relative"}}>Sign In:</h1>
-                <form>
-                    Username:
-                    <input type = "text" name = "username"></input>
-                    <br></br>
 
-                    Password:   
-                    <input type = "password" name = "password"></input>
-                    <br></br>
-                    <input type = "submit" name = "submit"></input>
+                <div class="d-flex justify-content-center">
+                    <h1 style={{color: "white", position: "relative"}}>Sign In:</h1>
+		        </div>
+
+		        <div class="d-flex justify-content-center">
+                    <form>
+                        <div class = "form-group">
+                            <label for = "username">Username:</label>
+                            <input type = "username" class = "form-group" placeholder = "enter username" id = "username"></input>
+                        </div>
+
+                        <div class = "form-group">
+                            <label for = "password">Password:</label>
+                            <input type = "password" class = "form-group" placeholder = "enter password" id = "password"></input>
+		                    
+		                </div>
+		        
+                <div class="d-flex justify-content-center">
+                    <button type = "submit" class = "btn btn-primary">Submit</button>
+                </div>
+		
+
+
                 </form>
-
-                
-
+		</div>
             </div>
 
         );
